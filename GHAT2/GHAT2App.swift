4a71022778922d3bdd092fb0674a53783324bb66
Scott Bennett <stt.btt@gmail.com>
2023-05-05 16:10:59 -0400

    Initial Commit

---
+++ GHAT2/GHAT2App.swift
@@ -0,0 +1,17 @@
+//
+//  GHAT2App.swift
+//  GHAT2
+//
+//  Created by Scott Bennett on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct GHAT2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
