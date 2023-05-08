b8eadd862171433b982240d59efdc4f55454062e
Scott Bennett <stt.btt@gmail.com>
2023-05-08 15:22:43 -0400

    Testing lint

--- GHAT2/ContentView.swift
+++ GHAT2/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName :"globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-                .font(.largeTitle)
+                .font(.title)
             Text("Hello, world!")
         }
         .padding()
