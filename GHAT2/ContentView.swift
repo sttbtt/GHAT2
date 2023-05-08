12c779bdc9dbb11c5779e354668b6fd00b6ee148
Scott Bennett <stt.btt@gmail.com>
2023-05-08 16:55:12 -0400

    Testing

--- GHAT2/ContentView.swift
+++ GHAT2/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundColor(.red)
+                .foregroundColor(.yellow)
                 .font(.title)
             Text("Hello, world!")
         }
